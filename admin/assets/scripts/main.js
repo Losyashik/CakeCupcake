1bb727d7abe2055f68650eadc24918c9885645b9
Losyashik <An.Shkenin@yandex.ru>
2024-04-09 15:09:31 +0300

    1

--- admin/assets/scripts/main.js
+++ admin/assets/scripts/main.js
@@ -23,28 +23,72 @@ document.querySelectorAll(".leftbar__link").forEach((item) => {
   })
 })
 
+function addImage(block, input) {
+  block.innerHTML = '';
+  let images = input.files;
+  [...images].forEach((file) => {
+    let reader = new FileReader();
+    reader.onload = function (e) {
+      block.insertAdjacentHTML('beforeEnd', `
+      <img src="${e.target.result}" />
+      `
+      )
+    };
+    reader.readAsDataURL(file);
+  });
+}
 
+document.querySelectorAll(".admin-form__input-file").forEach((item) => {
+  item.addEventListener('change', () => {
+    block = item.parentElement.firstElementChild;
+    console.log(block);
+    addImage(block, item);
+  })
+})
 
-
-document.querySelectorAll(".admin-form__label-content").forEach(item => {
+document.querySelectorAll(".admin-form__label-file").forEach(item => {
+  block = item.firstElementChild;
   ["dragstart", "dragenter"].forEach((e) => {
     document.addEventListener(e, () => {
-      item.classList.add("drag")
+      block.classList.add("drag")
+    })
+    item.addEventListener(e, () => {
+      block.classList.add("drag")
     })
   });
   ["dragleave", "dragend", "drop"].forEach((e) => {
     document.addEventListener(e, () => {
-      item.classList.remove("drag");
+      block.classList.remove("drag");
+    })
+    item.addEventListener(e, () => {
+      block.classList.remove("drag")
     })
   });
   item.addEventListener("drop", e => {
     console.log("drop");
-    let inp = e.currentTarget().parent;
+    let inp = item.lastElementChild;
     let inpFileList = inp.files;
+    let addedFiles = e.dataTransfer.files;
     let newFileList = new DataTransfer();
     for (var i = 0; i < inpFileList.length; i++) {
-      if (i != index) newFileList.items.add(inpFileList[i]);
+      newFileList.items.add(inpFileList[i]);
     }
+    [...addedFiles].forEach((file) => {
+      newFileList.items.add(file);
+    });
     inp.files = newFileList.files;
+    addImage(block, inp)
+  })
+})
+document.querySelectorAll(".admin-form").forEach(item => {
+  item.addEventListener("submit", async e => {
+    e.preventDefault();
+    let but = item.lastElementChild;
+    let body = new FormData(item);
+    data = await fetch(`./assets/backend/api/${but.name}/${but.value}/`, {
+      method: 'post',
+      body
+    }).then(text => text.json()).then(json => { return json })
+    console.log(data);
   })
 })
